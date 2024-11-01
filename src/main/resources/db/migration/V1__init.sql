823094694ef2bf25cdaa5af826829105f94a4dbf
FaberJoo <sjdk0805@gmail.com>
2024-11-01 20:36:45 +0900

    chore: 최초 sql 추가 #2

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -2,11 +2,22 @@
 CREATE TABLE member (
     id UUID PRIMARY KEY,
     username VARCHAR(255) NOT NULL UNIQUE,
+    role VARCHAR(20) NOT NULL DEFAULT 'GUEST',  -- GUEST, USER, ADMIN 역할 추가
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- 2. AuthSocial 테이블
+-- 2. AuthLocal 테이블
+CREATE TABLE auth_local (
+    id UUID PRIMARY KEY,
+    member_id UUID REFERENCES member(id) ON DELETE CASCADE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- 3. AuthSocial 테이블
 CREATE TABLE auth_social (
     id UUID PRIMARY KEY,
     member_id UUID REFERENCES member(id) ON DELETE CASCADE,
@@ -16,7 +27,7 @@ CREATE TABLE auth_social (
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- 3. Profile 테이블
+-- 4. Profile 테이블
 CREATE TABLE profile (
     id UUID PRIMARY KEY,
     member_id UUID REFERENCES member(id) ON DELETE CASCADE,
@@ -26,7 +37,7 @@ CREATE TABLE profile (
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- 4. Follow 테이블
+-- 5. Follow 테이블
 CREATE TABLE follow (
     id UUID PRIMARY KEY,
     follower UUID REFERENCES member(id) ON DELETE CASCADE,
@@ -34,7 +45,7 @@ CREATE TABLE follow (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- 5. Post 테이블 (수정됨)
+-- 6. Post 테이블
 CREATE TABLE post (
     id UUID PRIMARY KEY,
     member_id UUID REFERENCES member(id) ON DELETE CASCADE,
@@ -49,26 +60,26 @@ CREATE TABLE post (
     deleted_at TIMESTAMP -- Optional, 논리적 삭제
 );
 
--- 6. Tag 테이블
+-- 7. Tag 테이블
 CREATE TABLE tag (
     id UUID PRIMARY KEY,
     tag_name VARCHAR(32) NOT NULL
 );
 
--- 7. PostTag 테이블
+-- 8. PostTag 테이블
 CREATE TABLE post_tag (
     id UUID PRIMARY KEY,
     post_id UUID REFERENCES post(id) ON DELETE CASCADE,
     tag_id UUID REFERENCES tag(id) ON DELETE CASCADE
 );
 
--- 8. ReactionType 테이블
+-- 9. ReactionType 테이블
 CREATE TABLE reaction_type (
     id UUID PRIMARY KEY,
     type_name VARCHAR(32) NOT NULL
 );
 
--- 9. Reaction 테이블
+-- 10. Reaction 테이블
 CREATE TABLE reaction (
     id UUID PRIMARY KEY,
     post_id UUID REFERENCES post(id) ON DELETE CASCADE,
