cee1f26e5167b8ace7372ba70550bca80b3d8915
FaberJoo <sjdk0805@gmail.com>
2024-11-04 15:10:52 +0900

    refactor: 회원 가입 요청 dto 유효성 추가 #2

--- src/main/java/site/theneta/api/apiRest/v1/member/controller/request/SignupRequest.java
+++ src/main/java/site/theneta/api/apiRest/v1/member/controller/request/SignupRequest.java
@@ -6,22 +6,24 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record SignupRequest(
-        @NotBlank(message = "이메일은 필수입니다.")
-        @Email(message = "올바른 이메일 형식이 아닙니다.")
-        String email,
+    @NotBlank(message = "이메일은 필수입니다.")
+    @Email(message = "올바른 이메일 형식이 아닙니다.")
+    String email,
 
-        @NotBlank(message = "비밀번호는 필수입니다.")
-        @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$",
-                message = "비밀번호는 8자 이상, 영문, 숫자, 특수문자를 포함해야 합니다.")
-        String password,
+    @NotBlank(message = "비밀번호는 필수입니다.")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$",
+        message = "비밀번호는 8자 이상, 영문, 숫자, 특수문자를 포함해야 합니다.")
+    String password,
 
-        @NotBlank(message = "사용자 이름은 필수입니다.")
-        @Size(min = 2, max = 20, message = "사용자 이름은 2자 이상 20자 이하여야 합니다.")
-        @Pattern(regexp = "^[a-z0-9_]{4,20}$",
-                message = "사용자 이름은 영문 소문자, 숫자, 언더스코어만 사용 가능하며 4~20자 이내여야 합니다.")
-        String username,
+    @NotBlank(message = "사용자 이름은 필수입니다.")
+    @Size(min = 4, max = 20,
+        message = "사용자 이름은 4자 이상 20자 이하여야 합니다.")
+    @Pattern(regexp = "^[a-z0-9_]{4,20}$",
+        message = "사용자 이름은 영문 소문자, 숫자, 언더스코어만 사용 가능하며 4~20자 이내여야 합니다.")
+    String username,
 
-        @NotBlank(message = "이름은 필수입니다.")
-        @Size(min = 2, max = 30, message = "이름은 2자 이상 30자 이하여야 합니다.")
-        String name
+    @NotBlank(message = "이름은 필수입니다.")
+    @Size(min = 2, max = 30,
+        message = "이름은 2자 이상 30자 이하여야 합니다.")
+    String name
 ) { }
