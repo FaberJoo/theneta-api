969101d52b53422c0049db4fb1ecdda2c1c4f035
FaberJoo <sjdk0805@gmail.com>
2024-11-07 14:56:49 +0900

    feat: authLocal repository 이메일로 조회 #2

--- src/main/java/site/theneta/api/domain/member/repository/AuthLocalRepository.java
+++ src/main/java/site/theneta/api/domain/member/repository/AuthLocalRepository.java
@@ -1,5 +1,6 @@
 package site.theneta.api.domain.member.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import site.theneta.api.domain.member.entity.AuthLocal;
 
@@ -7,4 +8,6 @@ import java.util.UUID;
 
 public interface AuthLocalRepository extends JpaRepository<AuthLocal, UUID> {
   boolean existsByEmail(String email);
+
+  Optional<AuthLocal> findByEmail(String email);
 }
