70e431db84a3bd655b5b91ecc98528172dbb5608
FaberJoo <sjdk0805@gmail.com>
2024-11-07 14:57:40 +0900

    feat: authLocal selector 이메일로 조회 메소드 #2

--- src/main/java/site/theneta/api/domain/member/operator/AuthLocalSelector.java
+++ src/main/java/site/theneta/api/domain/member/operator/AuthLocalSelector.java
@@ -1,15 +1,31 @@
 package site.theneta.api.domain.member.operator;
 
+import java.util.Optional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
+import site.theneta.api.domain.member.entity.AuthLocal;
 import site.theneta.api.domain.member.repository.AuthLocalRepository;
+import site.theneta.api.global.error.ErrorCode;
+import site.theneta.api.global.error.exception.BusinessBaseException;
+import site.theneta.api.global.utils.either.Either;
 
 @Component
 @AllArgsConstructor
 public class AuthLocalSelector {
-    private final AuthLocalRepository authLocalRepository;
 
-    public boolean existsByEmail(String email) {
-        return authLocalRepository.existsByEmail(email);
+  private final AuthLocalRepository authLocalRepository;
+
+  public boolean existsByEmail(String email) {
+    return authLocalRepository.existsByEmail(email);
+  }
+
+  public Either<BusinessBaseException, AuthLocal> findByEmail(String email) {
+    Optional<AuthLocal> authLocal = authLocalRepository.findByEmail(email);
+    if (authLocal.isPresent()) {
+      return Either.right(authLocal.get());
+    } else {
+      return Either.left(
+          new BusinessBaseException(ErrorCode.INVALID_CERTIFICATION));
     }
+  }
 }
